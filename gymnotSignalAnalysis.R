f72760b393f0e8c6409d0b32babfc9082f5c51ab
carlosj-rr <cjrivera35@gmail.com>
2019-12-06 16:13:50 -0500

    intermediate step fixing the function that identifies high and low peaks

--- gymnotSignalAnalysis.R
+++ gymnotSignalAnalysis.R
@@ -31,10 +31,12 @@ secondsToSamples<-function(numSeconds,samplingRate) {
 clipIncompleteWaves<-function(inSignal) {
   signalSize<-length(inSignal@left)
 
-  maxPeaks<-which(inSignal@left > quantile(inSignal@left,c(0.99)))
-  minPeaks<-which(inSignal@left < quantile(inSignal@left,c(0.01)))
+#  maxPeaks<-which(inSignal@left > quantile(inSignal@left,c(0.99)))
+#  minPeaks<-which(inSignal@left < quantile(inSignal@left,c(0.01)))
+  maxPeaks<-which(inSignal@left > (max(inSignal@left) * 0.8))
+  minPeaks<-which(inSignal@left < (min(inSignal@left) * 0.8))
 
-  diffMaxPeaks<-diff(diffMaxPeaks)
+  diffMaxPeaks<-diff(maxPeaks)
   avgStepSize<-round(mean(diffMaxPeaks[diffMaxPeaks > mean(diffMaxPeaks)]))
   halfStep<-round(avgStepSize/2)
   sizeMinusHalfStep<-signalSize-halfStep
@@ -72,7 +74,8 @@ burnTails<-function(inSignal,burnSeconds=1) {
 }
 
 frequencyEstimate<-function(inSignal) {
-  peakRegionsIndices<-which(inSignal@left > quantile(inSignal@left,c(.99))) #Not usable across the board
+#  peakRegionsIndices<-which(inSignal@left > quantile(inSignal@left,c(.99))) #Not usable across the board
+  peakRegionsIndices<-which(inSignal@left > (max(inSignal@left) * 0.8))
   diffPRI<-diff(peakRegionsIndices)
   peakCount<-sum(diffPRI > 1)
   outValue<-round(peakCount/samplesToSeconds(length(inSignal),inSignal@samp.rate))
@@ -96,23 +99,23 @@ fourierTransfTable<-function(inSignal) {
   return(mainDF)
 }
 
-identifyPeaks<-function(inSignal,type="high") {
+findCorrespLowPeak<-(inSignal,peakRegionsIndicesHigh) {
+  
+}
+
+# TO DO: 1) Make function spit out a data frame that shows index and value of both high and low peaks, 2) Detect low peaks by looking for the lowest amplitude in the samples after a detected high peak (here, 'after' means half a frequency step, as in the variable 'halfStep' below).
+#identifyPeaks<-function(inSignal,type="high") {
   if (inSignal@stereo) {
     print("Input signal is stereo. Converting to mono.")
     inSignal<-monofy(inSignal)
   }
 
-# BUG: For individuals with really funky signals (e.g. 8-005 from Rio Zegla) break the functioning of this section. It was a problem of properly identifying the lower peaks when there was fluctuation within the peak. Fixed just after the for loop below, with the highestLowest variable.
-  peakRegionsIndicesHigh<-which(inSignal@left > quantile(inSignal@left,c(.99)))
-  peakRegionsIndicesLow<-which(inSignal@left < quantile(inSignal@left,c(.01)))
+  peakRegionsIndicesHigh<-which(inSignal@left > (max(inSignal@left) * 0.8))
+  diffMaxPeaks<-diff(peakRegionsIndicesHigh)
+  avgStepSize<-round(mean(diffMaxPeaks[diffMaxPeaks > mean(diffMaxPeaks)]))
+  halfStep<-round(avgStepSize/2)
 
-# BUG: If the chunk analyzed starts in the middle of a signal, it chooses a pulse as the low and then the high (including the silence in between). Possible solution: use only low-amplitude peaks that are in a higher index than the lowest-indexed high-amplitude peak, and use only high-amplitude peaks that are in a lower index than the highest-indexed low-amplitude peak. (## More or less fixed - see below ##).
-
-##### DOWN HERE ##### !!!!!    !!!!!!!!!    !!!!!!!!!!   !!!!!!!!!!!!!!!!
-
-# NEW PROBLEM: when a signal is not complete but both peaks are in there, it also makes a mistake. Find a way to remove any and all incomplete pulses at the extremities of the recording.
-  peakRegionsIndicesHigh<-subset(peakRegionsIndicesHigh, peakRegionsIndicesHigh < max(peakRegionsIndicesLow))
-  peakRegionsIndicesLow<-subset(peakRegionsIndicesLow, peakRegionsIndicesLow > min(peakRegionsIndicesHigh))
+  peakRegionsIndicesLow<-which(inSignal@left < (min(inSignal@left) * 0.8))
 
   if (type == "high") {
     peakRegionsIndices<-peakRegionsIndicesHigh
@@ -179,10 +182,9 @@ extractSingleWaveform<-function(inSignal,waveformNumber=1) {
 
 identifyLowerBound<-function(waveform,windowSize=10,stepSize=10) {
   lowestPeak<-min(which.max(waveform@left),which.min(waveform@left))
-plot(waveform)
   lowerBoundWindowStart<-lowestPeak-windowSize
   lowerBoundWindowEnd<-lowestPeak
-print(paste("Window Start:",lowerBoundWindowStart,"window ends:",lowerBoundWindowEnd))
+#print(paste("Window Start:",lowerBoundWindowStart,"window ends:",lowerBoundWindowEnd))
   testAgain=TRUE
   while (testAgain) {
 #print("Entered while loop")
@@ -240,14 +242,17 @@ findBounds<-function(waveform) { # NOTE: must contain a single waveform
   maxValIndex<-which.max(waveform@left)
   minValIndex<-which.min(waveform@left)
   minMaxInterval<-abs(maxValIndex-minValIndex)
-
+plot(waveform)
+abline(h=0,col="red")
   # Identify lower bound
   lowerBoundsVector<-c()
   for (i in (round(minMaxInterval/2)):minMaxInterval) {
     newEstimate<-identifyLowerBound(waveform,i,1)
+print(paste("Lower Bound Estimate Num ",i,": ",newEstimate))
     lowerBoundsVector<-c(lowerBoundsVector,newEstimate)
   }
   lowerBound<-round(median(lowerBoundsVector))
+abline(v=samplesToSeconds(lowerBound,waveform@samp.rate),col="blue")
   lowestPeak<-min(which.max(waveform@left),which.min(waveform@left))
   distFromLowerPeak<-lowestPeak-lowerBound
 
@@ -255,9 +260,11 @@ findBounds<-function(waveform) { # NOTE: must contain a single waveform
   upperBoundsVector<-c()
   for (i in (round(minMaxInterval/2)):round(minMaxInterval*1.5)) { # Larger window sizes give better reads for the smaller slope
     newEstimate<-identifyUpperBound(waveform,i,1)
+print(paste("Upper Bound Estimate Num ",i,": ",newEstimate))
     upperBoundsVector<-c(upperBoundsVector,newEstimate)
   }
   upperBound<-round(median(upperBoundsVector))
+abline(v=samplesToSeconds(upperBound,waveform@samp.rate),col="blue")
   upperPeak<-max(which.max(waveform@left),which.min(waveform@left))
   distFromUpperPeak<-upperBound-upperPeak
 
@@ -271,17 +278,17 @@ signalSummaryTable<-function(inSignal,howMany="ALL") {
     print("Input signal is stereo. Converting to mono.")
     inSignal<-monofy(inSignal)
   }
-
+#print("1")
   highPeaks<-identifyPeaks(inSignal,"high")
   lowPeaks<-identifyPeaks(inSignal,"low")
-  if (length(highPeaks) != length(lowPeaks)) {
-    print("Number of high vs low peaks does not match, this may bring troubles later. Please align sample better.")
-    return
+  if (length(highPeaks$peakIndex) != length(lowPeaks$peakIndex)) {
+    print("Number of high vs low peaks does not match, this may bring troubles later. Please check sample.")
+    quit()
   }
-
+#print("2")
   if (howMany == "ALL") {
     print("Sampling all waveforms in recorded signal")
-    peakNumbers<-c(2:(length(highPeaks$peakIndex)-1)) # Excluding first and last waveform as they may be incomplete
+#    peakNumbers<-c(2:(length(highPeaks$peakIndex)-1)) # Excluding first and last waveform as they may be incomplete
   } else if (is.numeric(howMany)) {
     howMany<-min(round(howMany),length(highPeaks$peakIndex))
     print(paste("Sampling",howMany,"random waveforms."))
@@ -293,7 +300,7 @@ signalSummaryTable<-function(inSignal,howMany="ALL") {
   } else {
     print(paste("Your input for 'howMany',",howMany,"was not understood"))
   }
-
+#print("3")
   resultsDF<-data.frame(Peak.Number=peakNumbers,Lower.Bound.Index=NA,P1.Index=highPeaks$peakIndex,Intercept.Index=NA,P2.Index=lowPeaks$peakIndex,Upper.Bound.Index=NA,P1.Value=highPeaks$value,P2.Value=lowPeaks$value,P1.P2.Ratio=NA,Time.To.P1=NA,Time.To.Switch=NA,Time.To.Repolarize=NA,Decay.Lambda=NA)
   resultsDF$P1.P2.Ratio<-abs(resultsDF$P1.Value/resultsDF$P2.Value)
 
@@ -302,27 +309,41 @@ signalSummaryTable<-function(inSignal,howMany="ALL") {
   upperBoundsVect<-c()
   lambdasVect<-c()
   inputIndex<-1
+#print("4")
   for (i in 1:length(rownames(resultsDF))) { # CAUTION: This section will produce incorrect results if signal is inverted. Avoid this by always using the channel that records the first peak as positive.
 print(paste(i,": Summarizing peak",randVector[i]))
     waveformNum<-i
     waveform<-extractSingleWaveform(inSignal,i)
     highPeak<-resultsDF$P1.Index[i]
     lowPeak<-resultsDF$P2.Index[i]
-    indexVector<-c(highPeak:lowPeak)
-    interceptIndex<-indexVector[which.min(abs(inSignal@left[highPeak:lowPeak]))]
-    interceptsVect[i]<-interceptIndex
+    indexVector<-tryCatch(c(highPeak:lowPeak),error=function(e) { return(NA) })
+
+    if ( is.na(indexVector) ) {
+      interceptsVect[i]<-NA
+    } else {
+      interceptIndex<-indexVector[which.min(abs(inSignal@left[highPeak:lowPeak]))]
+      interceptsVect[i]<-interceptIndex
+    }
 
     bounds<-findBounds(waveform)
-#print(bounds)
+print(paste("Bounds were found at:",bounds))
     lowerBound<-highPeak-bounds[1]
     upperBound<-lowPeak+bounds[2]
     lowerBoundsVect[i]<-lowerBound
     upperBoundsVect[i]<-upperBound
-
+#print("A")
     repolarizationSlice<-inSignal@left[lowPeak:upperBound] # Caution: this assumes the negative peak goes after the positive peak
+#print("B")
     repoSliceList<-list(sample=0:(length(repolarizationSlice)-1),value=repolarizationSlice)
-    fitModel<-nls(value ~ SSasymp(sample, Asym, R0, lrc), data=repoSliceList)
-    lambdasVect[i]<-exp(coef(fitModel)[["lrc"]])
+#print("C")
+    fitModel<-tryCatch(nls(value ~ SSasymp(sample, Asym, R0, lrc), data=repoSliceList),error=function(e) { return(NA) })
+#print(is.na(fitModel))
+    if ( is.na(fitModel)) {
+      lambdasVect[i]<-NA
+    } else {
+      lambdasVect[i]<-exp(coef(fitModel)[["lrc"]])
+    }
+#print("E")
   }
 
   resultsDF$Intercept.Index<-interceptsVect
@@ -368,205 +389,4 @@ totalStudyAnalysis<-function(inTable,pulsesPerIndiv=10) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-if (FALSE) { ############### START DELETED SECTION
-
-signalSlidingWindow<-function(inSignal,windowSize=10,stepSize=10) {
-  if (windowSize < length(inSignal) && stepSize < length(inSignal)) {
-    outputDF<-data.frame(Window.Start=NA,Window.Stop=NA,Max.Signal=NA,Min.Signal=NA,Mean.Signal=NA,Slope=NA,Is.Nonrandom=NA)
-    rowNumber<-1
-    windowStart<-1
-    windowStop<-windowSize
-    while (windowStart < length(inSignal)-windowSize+1) {
-#      print(paste("Doing step",windowStart,windowStop))
-      window<-inSignal@left[windowStart:windowStop]
-      maxSignal<-max(window)
-      minSignal<-min(window)
-      meanSignal<-mean(window)
-      sdSignal<-sd(window)
-      numSamples<-length(window)
-      timeVector<-c(1:numSamples)
-      randomPoints<-rnorm(numSamples,meanSignal,sdSignal)
-      tTest<-t.test(window,randomPoints)
-      isNonrandom<-tTest$p.value <= 0.05
-      print(isNonrandom)
-      linearModel<-lm(window ~ timeVector)
-      lmSlope<-linearModel$coefficients[2]
-      outputDF[rowNumber,]<-c(windowStart,windowStop,maxSignal,minSignal,meanSignal,lmSlope,isNonrandom)
-      windowStart<-windowStart+stepSize
-      windowStop<-min(windowStop+stepSize,length(inSignal))
-      rowNumber<-rowNumber+1
-    }
-  } else {
-    paste("Aborting: Window size",windowSize,"and/or step size",stepSize,"larger than size of dataset",length(inSignal))
-    return
-  }
-
-  return(outputDF)
-}
-
-
-
-signalSlidingWindow<-function(inSignal,windowSize=10,stepSize=10) {
-  if (windowSize < length(inSignal) && stepSize < length(inSignal)) {
-    outputDF<-data.frame(Window.Start=NA,Window.Stop=NA,Max.Signal=NA,Min.Signal=NA,Mean.Signal=NA,Slope=NA)
-    rowNumber<-1
-    windowStart<-1
-    windowStop<-windowSize
-    while (windowStart < length(inSignal)-windowSize+1) {
-#      print(paste("Doing step",windowStart,windowStop))
-      maxSignal<-max(inSignal@left[windowStart:windowStop])
-      minSignal<-min(inSignal@left[windowStart:windowStop])
-      meanSignal<-mean(inSignal@left[windowStart:windowStop])
-      timeVector<-c(1:length(inSignal@left[windowStart:windowStop]))
-      amplitudeVector<-inSignal@left[windowStart:windowStop]
-      linearModel<-lm(amplitudeVector ~ timeVector)
-      lmSlope<-linearModel$coefficients[2]
-      outputDF[rowNumber,]<-c(windowStart,windowStop,maxSignal,minSignal,meanSignal,lmSlope)
-      windowStart<-windowStart+stepSize
-      windowStop<-min(windowStop+stepSize,length(inSignal))
-      rowNumber<-rowNumber+1
-    }
-  } else {
-    paste("Aborting: Window size",windowSize,"and/or step size",stepSize,"larger than size of dataset",length(inSignal))
-    return
-  }
-
-  return(outputDF)
-}
-
-
-extractWaveformBracket<-function(inSignal,centerPulse=1,bracketSizeInPulses=10) {
-  highPeaksDF<-identifyPeaks(inSignal,type="high")
-  lowPeaksDF<-identifyPeaks(inSignal,type="low")
-  meanPulseLength<-round(mean(diff(highPeaksDF$peakIndex)))
-  meanDiffHighLows<-round(mean(abs(highPeaksDF$peakIndex - lowPeaksDF$peakIndex)))
-  padding<-round(meanPulseLength/2)
-  centerOfCenterPulse<-highPeaksDF$peakIndex[centerPulse] + round(meanDiffHighLows/2)
-
-  if (bracketSizeInPulses %% 2 == 0) {
-      pulsesBefore<-(bracketSizeInPulses/2)-1
-      pulsesAfter<-(bracketSizeInPulses/2)
-  } else {
-      pulsesBefore<-(bracketSizeInPulses/-1)/2
-      pulsesAfter<-(bracketSizeInPulses/-1)/2
-  }
-
-  bracketLowerLimit_pulses<-centerPulse-pulsesBefore
-  bracketUpperLimit_pulses<-centerPulse+pulsesAfter
-
-  totalPulses<-min(length(highPeaksDF$peakIndex),length(lowPeaksDF$peakIndex))
-  totalSamples<-length(inSignal@left)
-
-  if (bracketLowerLimit_pulses < 1) {
-    shift<-abs(bracketLowerLimit_pulses)
-    bracketLowerLimit_pulses<-1
-    bracketUpperLimit_pulses<-bracketUpperLimit_pulses+shift
-    paste("Caution: Bracket had to be shifted",shift,"pulses up.")
-  }
-
-  if (bracketUpperLimit_pulses > totalPulses) {
-    shift<-abs(bracketUpperLimit_pulses-totalPulses)
-    bracketUpperLimit_pulses<-totalPulses
-    bracketLowerLimit_pulses<-bracketLowerLimit_pulses-shift
-    paste("Caution: Bracket had to be shifted",shift,"pulses up.")
-  }
-
-  bracketLowerLimit_samples<-max(highPeaksDF$peakIndex[bracketLowerLimit_pulses]+round(meanDiffHighLows/2)-padding,0)
-  bracketUpperLimit_samples<-min(highPeaksDF$peakIndex[bracketLowerLimit_pulses]+round(meanDiffHighLows/2)+padding,totalPulses)
-
-  return(inSignal[bracketLowerLimit_samples:bracketUpperLimit_samples])
-
-}
-
-chooseBest<-function(inSignal,what="waveform",bracketSizeInPulses=10) {
-  allHighPeaks<-identifyPeaks(inSignal,type="high")
-  highestPeakIndex<-which.max(allHighPeaks$value)
-  highestPeak<-allHighPeaks[highestPeakIndex,]
-
-  if (what == "waveform") {
-    output<-extractSingleWaveform(inSignal,highestPeakIndex)
-  } else if (what == "bracket") {
-    lowNumBracket<-highestPeakIndex-pulsesBefore
-    highNumBracket<-highestPeakIndex+pulsesAfter
-    print(lowNumBracket)
-    print(highNumBracket)
-
-    if (lowNumBracket < 1) {
-      shift<-abs(lowNumBracket)
-      lowNumBracket<-1
-      highNumBracket<-highNumBracket+shift
-      paste("Caution: Bracket had to be shifted",shift,"pulses up.")
-    }
-
-    if (highNumBracket > length(allHighPeaks$value)) {
-      shift<-highNumBracket-length(allHighPeaks$value)
-      highNumBracket<-length(allHighPeaks$value)
-      lowNumBracket<-lowNumBracket-shift
-      paste("Caution: Bracket had to be shifted",shift,"pulses down.")
-    }
-    print(lowNumBracket)
-    print(highNumBracket)
-
-    output<-inSignal[lowNumBracket:highNumBracket]
-
-  } else {
-    paste("Argument 'what':",what,"not understood.\nPleas write \"waveform\" or \"bracket\"")
-    return()
-  }
-  return(output)
-}
-
-
-if (FALSE) {
-  peakRegionsIndices_high<-which(inSignal@left > quantile(inSignal@left,c(.99)))
-  peakRegionsIndices_low<-which(inSignal@left < quantile(inSignal@left,c(.01)))
-
-  diffPeakRegionsIndices_high<-diff(peakRegionsIndices_high)
-  diffPeakRegionsIndices_low<-diff(peakRegionsIndices_low)
-
-  stepVect_high<-c(peakRegionsIndices_high[1])
-  stepVect_low<-c(peakRegionsIndices_low[1])
-
-  inRowNum<-1
-
-  for (i in 1:length(diffPeakRegionsIndices_high)) {
-    if (diffPeakRegionsIndices_high[i] == 1) {
-      stepVect<-c(stepVect_high,peakRegionsIndices_high[i+1])
-    } else {
-      stepMaxIndex_high<-stepVect_high[which.max(inSignal@left[stepVect_high])]
-      stepMaxValue_high<-max(inSignal@left[stepVect_high])
-      resultsDF[inRowNum,c(1,2)]<-c(stepMaxIndex_high,stepMaxValue_high)
-      inRowNum<-inRowNum+1
-      stepVect_high<-c(peakRegionsIndices_high[i+1])
-    }
-  }
-
-  inRowNum<-1
-  for (i in 1:length(diffPeakRegionsIndices_low)) {
-    if (diffPeakRegionsIndices_low[i] == 1) {
-      stepVect<-c(stepVect_low,peakRegionsIndices_low[i+1])
-    } else {
-      stepMinIndex_low<-stepVect_low[which.min(inSignal@left[stepVect_low])]
-      stepMinValue_low<-min(inSignal@left[stepVect_low])
-      resultsDF[inRowNum,c(3,4)]<-c(stepMinIndex_low,stepMinValue_low)
-      inRowNum<-inRowNum+1
-      stepVect_low<-c(peakRegionsIndices_low[i+1])
-    }
-  }
-}
-
-
-} ####################################### END DELETED SECTION
 
